8894e4a5179c9a9ee3037e0eff4e4c7549eee08d
russkd <russ.dollinger@ingenuitor.com>
2015-05-05 21:03:29 -0700

    Create Ingredient, Recipe, and Recipe_Ingredient model. Build the relationship connections between the models. Create migrations for the foreign keys to go into Recipe_Ingredient model.

---
+++ db/migrate/20150506022106_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :description
+      t.text :ingredients
+      t.integer :time
+      t.text :directions
+
+      t.timestamps null: false
+    end
+  end
+end
