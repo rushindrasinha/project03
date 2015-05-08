5692fb9aabc3457bf94e16b3d47e822eafa1fc74
russkd <russ.dollinger@ingenuitor.com>
2015-05-08 15:33:11 -0700

    Fix seed file errors (two quotes had to be fixed). Add image column to recipe database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150506035141) do
+ActiveRecord::Schema.define(version: 20150508221703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150506035141) do
     t.text     "directions"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
   add_foreign_key "recipe_ingredients", "ingredients"
