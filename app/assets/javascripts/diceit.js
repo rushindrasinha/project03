0b26712bb428a94908fa0c5eb29a23916c437aa8
Gobind Tandon <gotandon@gmail.com>
2015-05-08 15:25:47 -0700

    starting api building

--- app/assets/javascripts/diceit.js
+++ app/assets/javascripts/diceit.js
@@ -9,7 +9,7 @@ function diceit() {
         var shuffle = function(array) {
                 var currentIndex = array.length,
                     temporaryValue, randomIndex;
-                // While there remain elements to shuffle... 
+                // While there remain elements to shuffle...
                 while (0 !== currentIndex) {
                     // Pick a remaining element...
                     randomIndex = Math.floor(Math.random() * currentIndex);
@@ -23,7 +23,7 @@ function diceit() {
             }
             // end shuffle array function
 
-        //assigning shuffled array to the variable so we can get the length    
+        //assigning shuffled array to the variable so we can get the length
         var shuffled_array = shuffle(array);
         console.log(shuffled_array);
         // this function resets the rejected array function
@@ -32,10 +32,11 @@ function diceit() {
             console.log("RESET REJECTED ARRAY: " + rejectedArray);
         }
 
-        //this is a work in process function 
+        //this is a work in process function
         var newSuggestion = function(){
 
         }
+
 
         //function to pick last index from shuffled array and suggest it to user
         var suggestion = function(shuffled_array) {
@@ -43,21 +44,20 @@ function diceit() {
                 var lastIndex = shuffled_array.length - 1;
                 suggestedRecipe = shuffled_array[lastIndex];
                 console.log(suggestedRecipe);
+                    // this pops the lastindex from the shuffled array
+                    shuffled_array.pop();
+                    // this push the last index to another array called rejected array.
+                    rejectedArray.push(suggestedRecipe);
+                    console.log("Shuffled array: " + shuffled_array);
+                    console.log("Rejected Array: " + rejectedArray);
+                    }else {
+                     reset();
+                     alert("refresh the page for now, we're under construction")
+                   }
+                }
                 
-            
 
-                // this pops the lastindex from the shuffled array 
-                shuffled_array.pop();
-                // this push the last index to another array called rejected array.
-                rejectedArray.push(suggestedRecipe);
-                console.log("Shuffled array: " + shuffled_array);
-                console.log("Rejected Array: " + rejectedArray);
-            } else {
-                reset();
-                alert("refresh the page for now, we're under construction");
-            }
-        }
-           
+
         suggestion(shuffled_array);
-        
+
     }
