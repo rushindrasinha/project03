0b3e35b214bf39de65dd5f7d99278bcc2e97052f
Gobind Tandon <gotandon@gmail.com>
2015-05-13 17:41:32 -0700

    adding rack cors gem & config middlewear

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,16 @@ module Project03
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+
+      origins "*"
+
+      resource "/api/*",
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options]
+        end
+    end
+
   end
 end
